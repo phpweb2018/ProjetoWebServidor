54360e2df3fe4e64c40cbb89ef62822fc6a2512b
Cristofer <cristoferviante07@gmail.com>
2018-11-05 21:32:05 -0200

    inclusão de bootstrap

--- dump.sql
+++ dump.sql
@@ -1,5 +1,5 @@
 create table clie(
-    clie_codi integer NOT NULL PRIMARY KEY, 
+    clie_codi integer NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     clie_nome varchar(150), 
     clie_emai varchar(100), 
     clie_nofa varchar(150), 
@@ -16,3 +16,16 @@ create table clie(
     clie_esta char(2),
     clie_obse varchar(300)
 );
+
+create table tick(
+    tick_ID integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tick_titulo varchar(100),
+    tick_cliente integer,
+    tick_responsavel varchar(100),
+    tick_situacao varchar(50),
+    tick_prioridade integer,
+    tick_criacao Date,
+    tick_departamento varchar(100)
+);
+
+alter table tick ADD CONSTRAINT tick_cliente FOREIGN KEY (tick_cliente) REFERENCES clie(clie_codi);
