1165290bfccd41db9660adf6761aa129df8a2414
leonaldo <phpweb2018@bol.com.br>
2018-11-05 19:17:45 -0200

    Inclusão de Script de criação da Tabela Cliente

--- dump.sql
+++ dump.sql
@@ -1,9 +1,18 @@
-CREATE TABLE produto
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(200) NOT NULL,
-    preco DOUBLE(10,2) NOT NULL,
-    quantidade INT NOT NULL,
-    descricao TEXT,
-    dataCadastro DATETIME DEFAULT NOW()
+create table clie(
+    clie_codi integer NOT NULL PRIMARY KEY, 
+    clie_nome varchar(150), 
+    clie_emai varchar(100), 
+    clie_nofa varchar(150), 
+    clie_tele varchar(14), 
+    clie_cnpj varchar(14), 
+    clie_tipo varchar(10),
+    clie_situ boolean, 
+    clie_dtco Date,
+    clie_logr varchar(100),
+    clie_nume integer,
+    clie_bair varchar(100),
+    clie_copl varchar(50),
+    clie_cida varchar(100),
+    clie_esta char(2),
+    clie_obse varchar(300)
 );
