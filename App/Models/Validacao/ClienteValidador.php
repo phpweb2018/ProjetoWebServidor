a86a149a9fb53c9539270abe850fd8058e26d0bd
leonaldo <phpweb2018@bol.com.br>
2018-11-06 18:08:36 -0200

    Finalizado Validações Do Cliente

--- App/Models/Validacao/ClienteValidador.php
+++ App/Models/Validacao/ClienteValidador.php
@@ -14,75 +14,75 @@ class ProdutoValidador{
        		  
 		if(empty($produto->getClie_nome()))
     {
-        $resultadoValidacao->addErro('nome',"Nome: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_nome',"Nome: Este campo não pode ser vazio");
     }
     
     if(empty($produto->getClie_emai()))
     {
-        $resultadoValidacao->addErro('preco',"Preço: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_emai',"Email: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_nofa()))
     {
-        $resultadoValidacao->addErro('quantidade',"Quantidade: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_nofa',"Nome Fantasia: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_tele()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_tele',"Telefone: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_cnpj()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_cnpj',"CNPJ: Este campo não pode ser vazio");
     }		  
     if(empty($produto->getClie_tipo()))
     {
-        $resultadoValidacao->addErro('nome',"Nome: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_tipo',"Tipo: Este campo não pode ser vazio");
     }
     
     if(empty($produto->getClie_situ()))
     {
-        $resultadoValidacao->addErro('preco',"Preço: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_situ',"Situação: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_dtco()))
     {
-        $resultadoValidacao->addErro('quantidade',"Quantidade: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_dtco',"Data do Contrato: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_logr()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_logr',"Logradouro: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_nume()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_nume',"Numero: Este campo não pode ser vazio");
     }		  
     if(empty($produto->getClie_bair()))
     {
-        $resultadoValidacao->addErro('nome',"Nome: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_bair',"Bairro: Este campo não pode ser vazio");
     }
     
     if(empty($produto->getClie_copl()))
     {
-        $resultadoValidacao->addErro('preco',"Preço: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_copl',"Complemento: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_cida()))
     {
-        $resultadoValidacao->addErro('quantidade',"Quantidade: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_cida',"Cidade: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_esta()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_esta',"UF-Estado: Este campo não pode ser vazio");
     }
 
     if(empty($produto->getClie_obse()))
     {
-        $resultadoValidacao->addErro('descricao',"Descrição: Este campo não pode ser vazio");
+        $resultadoValidacao->addErro('clie_obse',"Observações: Este campo não pode ser vazio");
     }
 
         return $resultadoValidacao;
