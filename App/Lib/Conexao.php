9f712eccbaab079e65a2dd349e3124d629fbfa4e
phpweb2018 <43307764+phpweb2018@users.noreply.github.com>
2018-11-22 02:21:56 -0200

    Update Conexao.php

--- App/Lib/Conexao.php
+++ App/Lib/Conexao.php
@@ -18,19 +18,19 @@ class Conexao
         $pdoConfig .= "dbname=".DB_NAME.";";
         
         
-        $bd = "pgsql:"
-		    . "host=ec2-54-83-50-145.compute-1.amazonaws.com;"
-		    . "dbname=d1j91br7qn9g5i;"
-		    . "user=vasysywxnlwooi;"
-		    . "port=5432;"
-		    . "sslmode=require;"
-		    . "password=2919bf6469b71cab763a5e1990cad2de481927ecb7614c9577e899e811d774a6";
+        //$bd = "pgsql:"
+        //	    . "host=ec2-54-83-50-145.compute-1.amazonaws.com;"
+	//	    . "dbname=d1j91br7qn9g5i;"
+	//	    . "user=vasysywxnlwooi;"
+	//	    . "port=5432;"
+	//	    . "sslmode=require;"
+	//	    . "password=2919bf6469b71cab763a5e1990cad2de481927ecb7614c9577e899e811d774a6";
 
 
         try { 
             if(!isset(self::$connection)){
-                //self::$connection =  new PDO($pdoConfig, DB_USER, DB_PASSWORD);
-                self::$connection =  new PDO($bd);
+                self::$connection =  new PDO($pdoConfig, DB_USER, DB_PASSWORD);
+                //self::$connection =  new PDO($bd);
                 self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             return self::$connection;
