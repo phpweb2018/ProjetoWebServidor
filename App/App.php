48fbbb8dc9f01887caf77e698c815ddc27413160
phpweb2018 <43307764+phpweb2018@users.noreply.github.com>
2018-11-22 01:24:05 -0200

    Update App.php

--- App/App.php
+++ App/App.php
@@ -16,7 +16,8 @@ class App
     public function __construct()
     {
         //define('APP_HOST'       , $_SERVER['HTTP_HOST'] . "/ProjetoWebServidor");
-        define('APP_HOST'       , "hasssolucoes.ga");
+        define('APP_HOST'       , $_SERVER['HTTP_HOST'] . "hasssolucoes.ga");
+        //define('APP_HOST'       , "hasssolucoes.ga");
         define('PATH'           , realpath('./'));
         define('TITLE'          , "Hass - Soluções");
         // define('DB_HOST'        , "localhost:3307");
